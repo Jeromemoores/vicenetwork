62ced48acd3776db5acd15bbce84e413db824127
Jerome Moores <30985199+Jeromemoores@users.noreply.github.com>
2022-05-15 11:29:09 -0500

    - Bootstrap Int

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,11 @@
-import React from 'react';
-import App from './App';
-import {createRoot} from 'react-dom/client';
+import React from 'react'
+import App from './App'
+import {createRoot} from 'react-dom/client'
+
+
+// CSS Global Imports
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 const container = document.getElementById('root')
 const root = createRoot(container)
